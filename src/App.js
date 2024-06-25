3a6294bb7451d6b81d7ef9f35584bc7786abb5a4
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-25 17:36:50 +0200

    Added Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Thabile P Mhlanga and is{" "}
+        <a
+          href="https://github.com/mhlangathabile/react-weather-forecast"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
