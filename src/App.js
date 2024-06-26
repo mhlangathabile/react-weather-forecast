a2dbb5a2a73c692d4ad392b8dbfbb3843b8adec2
mhlangathabile <mhlangathabile27@gmail.com>
2024-06-26 13:11:36 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Thabile P Mhlanga and is{" "}
-        <a
-          href="https://github.com/mhlangathabile/react-weather-forecast"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Thabile P Mhlanga and is{" "}
+          <a
+            href="https://github.com/mhlangathabile/react-weather-forecast"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
