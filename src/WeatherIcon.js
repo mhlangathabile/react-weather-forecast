8632ece53fd0729485def1dddd493eb54dc3711d
mhlangathabile <mhlangathabile27@gmail.com>
2024-10-10 12:16:39 +0200

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="pink"
+      color="black"
       size={props.size}
       animate={true}
     />
